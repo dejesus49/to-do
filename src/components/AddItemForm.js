37ee635ea9bbe42109976d2612a9d17688f462ac
dejesus49 <dejesus.49@osu.edu>
2023-06-21 00:20:20 -0400

    Initial commit

---
+++ src/components/AddItemForm.js
@@ -0,0 +1,12 @@
+const AddItemForm = () => {
+    return (
+        <form>
+            <label>
+                <input type="text"/>
+            </label>
+            <input type="submit"/>
+        </form>
+    );
+};
+
+export default AddItemForm;
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+const List = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    List Item 1
+                </li>
+                <li>
+                    List Item 2
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default List;
